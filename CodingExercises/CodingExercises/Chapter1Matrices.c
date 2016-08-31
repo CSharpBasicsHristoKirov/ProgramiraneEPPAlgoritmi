df9e6a3748652d6733845e3e720ee1ddccd3acf3
CSharpBasicsHristoKirov <hristo.kirov85@gmail.com>
2016-08-31 22:39:58 +0300

    First Exercise added

---
+++ CodingExercises/CodingExercises/Chapter1Matrices.c
@@ -0,0 +1,46 @@
+/*
+	TITLE          Operations with Matrices		Chapter1Matrices.c 
+	COMMENT
+		Objective: Fill a matrix in the format:
+
+					0 20 19 17 14
+					1 0  18 16 13
+					2 5  0  15 12
+					4 7  9  10 0
+			Input: -
+		   Output: -
+		   Author: Chris B. Kirov
+		     Date: 31.08.2016
+
+*/ 
+#include <stdio.h>
+
+unsigned const int MAX = 5;
+
+void fillMatrix (unsigned a [][MAX], unsigned n);
+
+//-----------------------------------------------
+int main()
+{
+	unsigned a[MAX][MAX];
+
+	return 0;
+}
+//-----------------------------------------------
+
+/*
+	Function: fillMatrix()
+
+	It fills the elements of the 2D matrix: a
+	in the followint format:
+
+	0 20 19 17 14
+	1 0  18 16 13
+	2 5  0  15 12
+	4 7  9  10 0
+*/
+void fillMatrix (unsigned a [][MAX], unsigned n)
+{
+
+}
+
